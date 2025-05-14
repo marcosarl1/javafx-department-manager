7189afeb15beafd3ffc8e959ded56fdeb809c888
Marcos Júnior <marcosajr11@gmail.com>
2025-05-13 22:08:06 -0300

    feat: About View

--- src/main/java/com/workshopjavafxjdbc/Main.java
+++ src/main/java/com/workshopjavafxjdbc/Main.java
@@ -8,17 +8,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene scene;
+
     @Override
     public void start(Stage stage) throws IOException {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
+            scene = new Scene(fxmlLoader.load());
             stage.setTitle("Sample JavaFX application");
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getScene() {
+        return scene;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/workshopjavafxjdbc/controller/MainViewController.java
+++ src/main/java/com/workshopjavafxjdbc/controller/MainViewController.java
@@ -1,9 +1,18 @@
 package com.workshopjavafxjdbc.controller;
 
+import com.workshopjavafxjdbc.Main;
+import com.workshopjavafxjdbc.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,11 +39,28 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemAboutAction() {
-
+        loadView("/com/workshopjavafxjdbc/About.fxml");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    private synchronized void loadView(String absoluteName) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = fxmlLoader.load();
+
+            Scene scene = Main.getScene();
+            VBox mainVBox = (VBox) ((ScrollPane) scene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().getFirst();
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
