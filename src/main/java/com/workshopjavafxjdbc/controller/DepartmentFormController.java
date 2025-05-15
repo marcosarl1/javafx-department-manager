f23a63f04e380cf17b73e2a719b4a8b7690dc663
Marcos Júnior <marcosajr11@gmail.com>
2025-05-15 18:45:58 -0300

    feat: DepartmentForm Controller

---
+++ src/main/java/com/workshopjavafxjdbc/controller/DepartmentFormController.java
@@ -0,0 +1,50 @@
+package com.workshopjavafxjdbc.controller;
+
+import com.workshopjavafxjdbc.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    public void onBtnSaveAction() {
+
+    }
+
+    @FXML
+    public void onBtCancelAction() {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+}
